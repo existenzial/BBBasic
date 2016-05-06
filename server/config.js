4355d5b209c1c748e835afa4009e369a0e6bff8a
Jazz Lyles <ripley36706@gmail.com>
2016-05-05 20:34:36 -0700

    fixed server config module.exports

--- server/config.js
+++ server/config.js
@@ -1,13 +1,16 @@
 //Server configuration
-app.configure( function(){
+module.exports = function(app){
 
-	app.use( express.bodyParser() ); //parses request body and populates it
-	app.use( express.methodOverride() ); //checks requests for HTTP overrides (e.g.: PUT or DELETE requests)
-	app.use( app.router ); //lookup routes based on HTTP method & url requested
-	app.use( express.errorHandler({
-	//shows errors while in "Development" mode
-		dumpExceptions: true, 
-		showStack: true 
-	}));
+	app.configure( function(){
 
-});
+		app.use( express.bodyParser() ); //parses request body and populates it
+		app.use( express.methodOverride() ); //checks requests for HTTP overrides (e.g.: PUT or DELETE requests)
+		app.use( app.router ); //lookup routes based on HTTP method & url requested
+		app.use( express.errorHandler({
+		//shows errors while in "Development" mode
+			dumpExceptions: true, 
+			showStack: true 
+		}));
+
+	});
+};
